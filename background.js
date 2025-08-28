8d05774ea7aa8eb206d7438629fe87bb994fc18b
Ria <agveliina@gmail.com>
2025-08-28 16:38:35 +0300

    Cleaned up popup.css to look a little better. Added a keyboard shortcut for toggling readability mode. (Ctrl+Shift+Q) Added background.js to handle the keyboard shortcut.

---
+++ background.js
@@ -0,0 +1,9 @@
+console.log("Background script loaded");
+
+chrome.commands.onCommand.addListener((command) => {
+    if (command === "toggle-readability") {
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "toggle"});
+        });
+    }
+});
